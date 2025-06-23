3508cca0ff264ef63f14d9fbf9c7ce59e957a910
biswajit-9776 <biswajitpatt139@gmail.com>
2025-06-23 23:35:20 +0530

    Explored lifetimes of mutable borrows during slices as references
    Signed-off-by: biswajit-9776 <biswajitpatt139@gmail.com>

--- projects/references/src/main.rs
+++ projects/references/src/main.rs
@@ -1,21 +1,55 @@
 fn main() {
     // Mutable and Immutable References
-    let mut s = String::from("hello");
-    func(&mut s);
-    println!("{}", s);
+    let mut s0 = String::from("hello");
+    func(&mut s0);
+    println!("{}", s0);
 
-    let mut s = String::from("hello");
-    let r1 = &mut s;
+    // Cannot borrow a variable as immutable and mutable at same time
+    // i.e, we can't use the reference and variable in same scope
+    let mut s1 = String::from("hello1");
+    let r1 = &mut s1; // here borrowed as mutable
     r1.push_str(", world!");
-    println!("{:p}", r1);
+    s1.clear(); // here borrowed as immutable
 
-    let m = 42;
-    let r2 = &m;
+    let s1 = String::from("hello1");
+    let r1 = &s1[..]; // here borrowed as mutable
+    println!("{}", r1); // here mutable borrow is used once and not after so Rust thinks its lifetime is over
+    println!("{}", s1);
+
+    let mut s1 = String::from("hello1");
+    let r1 = &mut s1;
+    r1.push_str(", world!");
+    println!("{}", r1); // here borrowed as mutable
+    println!("{}", s1); // here borrowed as immutable so okay as Rust thinks r1's lifetime is over
+    // println!("{}", r1); // here mutable borrow used again which Rust thought has its lifetime over
+
+    let s2 = 42;
+    let r2 = &s2;
     println!("{}", *r2);
 
     let s3 = String::from("hello");
-    let r1 = &s3;
-    println!("{}", r1);
+    let r3 = &s3;
+    println!("{}", r3);
+
+    let s4 = String::from("hello4");
+    let mut r4 = s4;
+    r4.push_str(", world!");
+    println!("{}", r4);
+
+    let s5 = String::from("hello5");
+    let mut r5 = s5;
+    r5.push_str(", world!");
+    println!("{}", r5);
+
+    let mut s6 = String::from("hello6");
+    let r6 = &mut s6;
+    r6.push_str(", world!");
+    println!("{}", r6);
+
+    let s7 = String::from("hello7");
+    let r7 = &s7;
+    println!("{}", r7);
+    println!("{}", s7);
 
     let s = "hellojediejfiejfj ejfiejfij";
     let bytes = s.as_bytes();
