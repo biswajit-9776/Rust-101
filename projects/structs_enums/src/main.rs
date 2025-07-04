adf6c3cfb6c3a8e6540a1f12b82441117fdfb0fe
biswajit-9776 <biswajitpatt139@gmail.com>
2025-07-04 21:06:42 +0530

    Finished references, structs and enums
    Signed-off-by: biswajit-9776 <biswajitpatt139@gmail.com>

--- projects/structs_enums/src/main.rs
+++ projects/structs_enums/src/main.rs
@@ -1,3 +1,4 @@
+use std::net::{IpAddr, Ipv4Addr};
 fn main() {
     let mut user = User {
         name: String::from("Patrick"),
@@ -6,8 +7,23 @@ fn main() {
         active: true,
     };
     user.name = String::from("Ana");
+    user.email = String::from("Ana@gmail.com");
+    user.sign_in_count = 3;
+    user.active = true;
+
     println!("{:?}", user);
+
+    user.add_by_five();
+
     println!("{:#?}", user);
+    println!("{:#?}", User::BITS);
+
+
+    let a = Some(5);
+    let b: Option<i32> = None;
+    println!("{:?}", a);
+    let b = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
+    println!("{:?}", b);
 }
 #[derive(Debug)]
 struct User {
@@ -18,7 +34,13 @@ struct User {
 }
 
 impl User {
-    fn addByFive(&self) -> u64{
+    fn add_by_five(&self) -> u64{
         self.sign_in_count + 5
     }
+    pub const BITS: u32 = 32;
+}
+
+enum IPAddr{
+    V4(String),
+    V6(String),
 }
